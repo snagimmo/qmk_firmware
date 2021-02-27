b907a79fc0860295b72648315890d1da3fe1b6df
snagimmo <31765952+snagimmo@users.noreply.github.com>
2021-02-27 23:15:54 +0900

    changed how to switch layers

--- keyboards/claw44/keymaps/snagimmo/keymap.c
+++ keyboards/claw44/keymaps/snagimmo/keymap.c
@@ -28,8 +28,12 @@ enum layer_number {
     _NUMPAD,
 };
 
+#define KC_TO0 TO(_QWERTY)         // raise(layer1)
+#define KC_TO1 TO(_RAISE)          // raise(layer1)
+#define KC_TO2 TO(_LOWER)          // lower(layer2)
+#define KC_TO3 TO(_NUMPAD)         // numpad(layer3)
 #define KC_LT1 LT(_RAISE, KC_NO)   // raise(layer1)
-#define KC_LT2 LT(_LOWER, KC_NO)   // lower(layer2)
+#define KC_LT2 LT(_LOWER, KC_CAPS) // lower(layer2)
 #define KC_LT3 LT(_NUMPAD, KC_NO)  // numpad(layer3)
 #define KC_C_LBR LCTL_T(KC_LBRC)   // l-ctrl
 #define KC_C_RBR RCTL_T(KC_RBRC)   // r-ctrl
@@ -54,35 +58,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
         KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL , KC_DEL ,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-        KC_CAPS, KC_HOME, KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_END , KC_BSLS,
+        KC_APP , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_NO  , KC_BSLS,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_NO  , KC_NO  , KC_NO  , KC_INS , KC_NO  ,     _______, KC_PGDN, KC_PGUP, KC_NO  , KC_NO  , KC_RSFT,
+        _______, KC_NO  , KC_NO  , KC_NO  , KC_INS , KC_NO  ,     KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_NO  , _______,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                          KC_NO  , KC_LALT, KC_LCTL, KC_G_WN,     KC_NO  , KC_RCTL, KC_RALT, KC_NO
+                          KC_TO2 , KC_LALT, KC_LCTL, KC_G_WN,     KC_NO  , KC_RCTL, KC_RALT, KC_NO
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 
     [_LOWER] = LAYOUT( \
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-        KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_NO  ,
+        KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-        KC_LT3 , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NO  ,
+        KC_TO0 , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NO  ,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_F11 , KC_F12 , KC_NO  , KC_NO  , KC_NO  ,     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_RSFT,
+        _______, KC_F11 , KC_F12 , KC_NO  , KC_NO  , KC_NO  ,     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , _______,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                          KC_NO  , KC_LALT, KC_LCTL, KC_NO  ,     KC_APP , KC_RCTL, KC_RALT, KC_NO
+                          KC_CAPS, KC_LALT, KC_LCTL, _______,     _______, KC_RCTL, KC_RALT, KC_TO3
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 
     [_NUMPAD] = LAYOUT( \
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_PSLS, KC_P7  , KC_P8  , KC_P9  , KC_EQL , _______,
+        KC_NO  , RESET  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_PSLS, KC_P7  , KC_P8  , KC_P9  , KC_EQL , _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_PAST, KC_P4  , KC_P5  , KC_P6  , KC_NO  , KC_NO  ,
+        KC_TO0 , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_PAST, KC_P4  , KC_P5  , KC_P6  , KC_NO  , KC_NO  ,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_PMNS, KC_P1  , KC_P2  , KC_P3  , KC_PDOT, KC_RSFT,
+        _______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,     KC_PMNS, KC_P1  , KC_P2  , KC_P3  , KC_PDOT, _______,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                          KC_NO  , KC_LALT, KC_LCTL, KC_NO  ,     KC_NO  , KC_RCTL, KC_PPLS, KC_P0
+                          KC_CAPS, KC_LALT, KC_LCTL, _______,     _______, KC_RCTL, KC_PPLS, KC_P0
     //                  `--------+--------+--------+--------'   `--------+--------+--------+--------'
     ),
 };
